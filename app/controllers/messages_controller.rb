c2894e8dd2f6c02255154016f08df3448081eada
nanomanon56 <manon.l.jean@gmail.com>
2017-11-28 16:34:50 -0500

    destry and edit added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :find_message, only:[:show, :edit, :update, :destory]
+
   def index
     @messages = Message.all.order("created_at DESC")
   end
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -17,10 +18,30 @@ class MessagesController < ApplicationController
   else
     render 'new'
    end
+ end
+
+   def edit
+   end
+
+  def update
+    if @message.update(message_params)
+      redirect_to message_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destory
+    @message.destory
+    redirect_to root_path 
   end
 
   private
   def message_params
     params.require(:message).permit(:title,:description)
  end
-end
+
+   def find_message
+     @message = Message.find(params[:id])
+   end
+ end
